c93a756723e74e3a436a2d8d0ce9ab06878bab14
Shubham Patil <sup31patil@gmail.com>
2026-03-23 08:05:57 +0530

    Initial commit: DSA Python project

---
+++ src/Arrays/array_duplicateValue.py
@@ -0,0 +1,9 @@
+num=[1,2,3,3,2]
+if len(set(num)) == len(num):
+    print("No duplicate values")
+else:
+    print("Duplicate values are present")
+    print("Unique values:", set(num))
+    seen = set()
+    duplicates = list(x for x in num if x in seen or seen.add(x))
+    print("Duplicate values:", duplicates)
